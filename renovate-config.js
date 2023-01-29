ce9f18af568fb07ae49ee75f6374bfd6365bf9ee
Balázs Varga <warnyul@gmail.com>
2023-01-29 09:32:49 +0000

    Add renovate-config.js

---
+++ renovate-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    branchPrefix: 'test-renovate/',
+    username: 'renovate-release',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    onboarding: false,
+    platform: 'github',
+    includeForks: true,
+    dryRun: 'full',
+    packageRules: [
+      {
+        description: 'lockFileMaintenance',
+        matchUpdateTypes: [
+          'pin',
+          'digest',
+          'patch',
+          'minor',
+          'major',
+          'lockFileMaintenance',
+        ],
+        dependencyDashboardApproval: false,
+        stabilityDays: 0,
+      },
+    ],
+  };
