5709a2dc059fa50f03f322533f4b4e55587e58c2
Balázs Varga <balazs.varga@apter.tech>
2023-02-08 21:29:34 +0000

    Code format

--- renovate-config.js
+++ renovate-config.js
@@ -1,29 +1,29 @@
 module.exports = {
-    branchPrefix: 'renovate/',
-    username: 'renovate-self-hosted-apter[bot]',
-    gitAuthor: 'Renovate Self Hosted Apter <123456+renovate-self-hosted-apter[bot]@users.noreply.github.com>',
-    onboarding: true,
-    onboardingConfig: {
-        "$schema": "https://docs.renovatebot.com/renovate-schema.json",
-        "extends": [
-            "github>apter-tech/renovate-config#v0.2.0"
-        ]
+  branchPrefix: 'renovate/',
+  username: 'renovate-self-hosted-apter[bot]',
+  gitAuthor: 'Renovate Self Hosted Apter <123456+renovate-self-hosted-apter[bot]@users.noreply.github.com>',
+  onboarding: true,
+  onboardingConfig: {
+    "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+    "extends": [
+      "github>apter-tech/renovate-config#v0.2.0"
+    ]
+  },
+  platform: 'github',
+  includeForks: true,
+  packageRules: [
+    {
+      description: 'lockFileMaintenance',
+      matchUpdateTypes: [
+        'pin',
+        'digest',
+        'patch',
+        'minor',
+        'major',
+        'lockFileMaintenance',
+      ],
+      dependencyDashboardApproval: false,
+      stabilityDays: 0,
     },
-    platform: 'github',
-    includeForks: true,
-    packageRules: [
-      {
-        description: 'lockFileMaintenance',
-        matchUpdateTypes: [
-          'pin',
-          'digest',
-          'patch',
-          'minor',
-          'major',
-          'lockFileMaintenance',
-        ],
-        dependencyDashboardApproval: false,
-        stabilityDays: 0,
-      },
-    ],
-  };
+  ],
+};
